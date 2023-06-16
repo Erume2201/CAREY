daacdf1bd9e4ae1b27a6d2f0213615d326e1dd62
Erume2201 <135057750+Erume2201@users.noreply.github.com>
2023-06-15 20:15:14 -0600

    se genera el login
    en este apartado se codifico un prototipo de login

--- index.php
+++ index.php
@@ -3,16 +3,27 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>::KAREY::</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
+    <title>.:KAREY:.</title>
+    <link rel='stylesheet' type='text/css' 
+    href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css'>
+
+    <!--icono en la pestaña-->
+    <link rel="icon" href="image/iconoN.ico" type="image/x-icon">
+
+    
 </head>
 <body>
-    <div class="container"> 
-        <div>
-            <h1>hola mundo</h1>
-        </div>
-    </div>  
+    <!--Cargo el archivo de operacion para mandar a llamar LA VISTA LOGIN-->
+    <?php
+    include_once("view/login/login.html");
+    ?>
+
+
+    <script type ="text/javascript" 
+    src ="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js">
+    </script>
+    <!--agregamos el poper y js-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
 </body>
 </html>
