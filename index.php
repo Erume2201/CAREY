16518b8b99e20f43f7ce94a0c6d71be5578c2198
Erume2201 <135057750+Erume2201@users.noreply.github.com>
2023-06-16 11:40:43 -0600

    cambios uno

--- index.php
+++ index.php
@@ -16,10 +16,9 @@
 <body>
     <!--Cargo el archivo de operacion para mandar a llamar LA VISTA LOGIN-->
     <?php
-    include_once("view/login/login.html");
+    include_once("view/login/login.php");
+   // include_once("view/menu/menu.php");
     ?>
-
-
     <script type ="text/javascript" 
     src ="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js">
     </script>
---
+++ view/login/login.php
@@ -0,0 +1,60 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <title>.:LOGIN CAREY:.</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+  <script src='main.js'></script>
+
+</head>
+
+<body class="bg-dark d-flex justify-content-center align-items-center vh-100">
+  <div class="bg-white p-5 rounded-5 text-secondary shadow" style="width: 25rem">
+    <div class="d-flex justify-content-center">
+      <img src="" alt="login-icon" style="height: 7rem" />
+    </div>
+    <div class="text-center fs-1 fw-bold">Login</div>
+    <div class="input-group mt-4">
+      <div class="input-group-text bg-info">
+        <img src="https://cdn-icons-png.flaticon.com/512/6073/6073873.png" alt="username-icon" style="height: 2rem" />
+      </div>
+      <input class="form-control bg-light" type="text" placeholder="Usuario" />
+    </div>
+    <div class="input-group mt-1">
+      <div class="input-group-text bg-info">
+        <img src="./assets/image/icon-contrasena.png" alt="password-icon" style="height: 2rem" />
+      </div>
+      <input class="form-control bg-light" type="password" placeholder="Contraseña" />
+    </div>
+    <div class="d-flex justify-content-around mt-1">
+      <div class="d-flex align-items-center gap-1">
+      </div>
+      <div class="pt-1">
+        <a href="#" class="text-decoration-none text-info fw-semibold fst-italic" style="font-size: 0.9rem">¿Olvidaste
+          tu contraseña?</a>
+      </div>
+    </div>
+    
+    <div>
+      <button class="btn btn-info text-white w-100 mt-4 fw-semibold shadow-sm" >
+        Iniciar Sesión
+      </button>
+
+    </div>
+    <div class="d-flex gap-1 justify-content-center mt-1">
+
+    </div>
+</body>
+<script src="/controller/javascript/acces_menu.js" type="module">
+
+
+</script>
+
+</html>
