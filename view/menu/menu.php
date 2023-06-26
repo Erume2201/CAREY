2a3944073f26e1cc808491a2ef8958ca744730c2
Erume2201 <135057750+Erume2201@users.noreply.github.com>
2023-06-25 22:04:31 -0600

    boton menú para la responsive
    se agregó el boton para cuando uno esté en dispositivos moviles

--- view/menu/menu.php
+++ view/menu/menu.php
@@ -9,6 +9,13 @@
         <!--Agregamos el icono al navbar-->
         <img src="./assets/image/logo_carey.png" alt="" width="15%"></img>
         <ul></ul>
+        <!--boton de lo responsivo-->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+        <!--termina el boton-->
         <!--Comienza el menú principal-->
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
