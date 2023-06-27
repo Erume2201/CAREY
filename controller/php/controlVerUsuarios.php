49c07a695ed0f7afd1f73290d3ef817dad030887
JPech213 <119896157+JPech213@users.noreply.github.com>
2023-06-27 09:56:36 -0600

    Eliminar usuario

--- controller/php/controlVerUsuarios.php
+++ controller/php/controlVerUsuarios.php
@@ -1,72 +1,33 @@
 <?php 
+// Obtener los valores enviados por el formulario
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require '../../controller/php/CRUD.php'; 
 
-	$SQL = "SELECT * FROM usuarios";
-    $resultado = Consulta($SQL);
-    $filas = count($resultado);
-    if($filas == 0){
-    	echo '
-    			<td>Tablas sin xpath_register_ns_auto(xpath_context)</td>
-    		';
-    } else{
-    	if (!empty($resultado)) {
-        for ($i = 0; $i < $filas; $i++) {
-        	$id = $resultado[$i]['id_usuarios'];
-	        echo '
-	            <tr>
-	            <form action=""controller/php/controlVerUsuarios.php"" method="POST">
-	                <td>'.$resultado[$i]['nombre_completo'].'</td>
-	                <td>'.$resultado[$i]['nombre_usuario'].'</td>
-	                <td>'.$resultado[$i]['telefono_usuario'].'</td>
-	                <td>'.$resultado[$i]['correo'].'</td>
-	                <td>'.$resultado[$i]['contrasena'].'</td>
-	                <td>'.$resultado[$i]['rol_usuario'].'</td>
-	                <input type="hidden" class="bt-5" id="dato" 
-                   		name="dato"
-                  		value="'.$resultado[$i]['id_usuarios'].'" autocomplete="off" 
-					>
-					<input type="hidden" class="bt-5" id="dato" 
-                   		name="modi"
-                  		value="'.$resultado[$i]['nombre_usuario'].'" autocomplete="off" 
-					>
-  	                <td><button type="submit " name="delete" id="delete" class="btn btn-primary" style="width: 40px; height: 40px; display: flex; justify-content: center; align-items: center;">
-	                		<img src="assets/image/delete_icon.png" width="35" height="35">
-	                </button>
-	                </td>
-	                <td>
-		                <button type="submit" name="udate" id="udate" class="btn btn-primary" style="width: 40px; height: 40px; display: flex; justify-content: center; align-items: center;">
-		                		<img src="assets/image/update_folder_icon.png" width="35" height="35">
-		                </button>
-	                </td>
-	            </form>
-	            </tr>
-	        ';}
-    } else {              
-    	echo '<div style="color: red;">Error al conectar a la base de datos</div>';
-    }
-    }              
-    
-
-
-if (isset($_POST['dato'])) {
+	if (isset($_POST['delete'])) {
 		$dato = $_POST['dato'];
 		//echo $dato;
-		$SQL  = "DELETE FROM usuarios WHERE id_usuarios = '$dato';";
-	    $resultadoDelete = EliminarDato($SQL);
-	    if ($resultadoDelete) {
-	        echo "<script>window.location = '../karey/index.php?module=usuario'</script>";
-	        ?>
-	        	<div class="alert alert-danger" role="alert">
-					Usuario Eliminado
+		$SQL = "DELETE FROM usuarios WHERE id_usuarios = '$dato'";
+		$resultado = EliminarDato($SQL);
+		
+		if ($resultado) {
+			?>
+			   	<div class="alert alert-success" role="alert">
+					  El usuario se elimino correctamente!
 				</div>
-	        <?php
-	    }else{
-	        echo "<script>window.location = '../karey/index.php?module=usuario'</script>";
-	    }
-}
+			<?php
+			echo "<script>window.location = '../../index.php?module=usuario&Delete=Borrado'</script>";
+			
+		} else{
+			echo "<script>window.location = '../../index.php?module=usuario&Delete=NoBorrado'</script>";
+			?>
+			   	<div class="alert alert-danger" role="alert">
+				  Algo salio mal al momento de eliminar el usuario!
+				</div>
+			<?php
+		}
+	}
 
-if(isset($_POST['delete'])){
-  		$valor = $_POST['delete'];
-  		echo $valor;
-  	}
 
 ?>
--- modules/moduloUsuario/verUsuarios.php
+++ modules/moduloUsuario/verUsuarios.php
@@ -19,9 +19,39 @@
 		  </thead>
 		  <tbody>
 		    <?php
-				//include("controller/php/conexion.php");
-				include("controller/php/controlVerUsuarios.php");
-			?>
+				$SQL = "SELECT * FROM usuarios";
+    			$resultado = Consulta($SQL);
+    			foreach($resultado as $fila){ ?>
+    				<form action="controller/php/controlVerUsuarios.php" method="POST">
+    				<tr>
+    					<td><?php echo $fila['nombre_completo']; ?></td>
+    					<td><?php echo $fila['nombre_usuario']; ?></td>
+    					<td><?php echo $fila['telefono_usuario']; ?></td>
+    					<td><?php echo $fila['correo']; ?></td>
+    					<td><?php echo $fila['contrasena']; ?></td>
+    					<td><?php echo $fila['rol_usuario']; ?></td>
+    					<input type="hidden" class="bt-5" id="dato" 
+                   		name="dato"
+                  		value="<?php echo $fila['id_usuarios'];?>" autocomplete="off" 
+						>
+						<input type="hidden" class="bt-5" id="modi" 
+	                   		name="modi"
+	                  		value="<?php echo $fila['id_usuarios'];?>'" autocomplete="off" 
+						>
+						<td><button type="submit " name="delete" id="delete" class="btn btn-primary" style="width: 40px; height: 40px; display: flex; justify-content: center; align-items: center;">
+	                		<img src="assets/image/delete_icon.png" width="35" height="35">
+	                	</button>
+	                	</td>
+						<td>
+		                <button type="submit" name="udate" id="udate" class="btn btn-primary" style="width: 40px; height: 40px; display: flex; justify-content: center; align-items: center;">
+		                		<img src="assets/image/update_folder_icon.png" width="35" height="35">
+		                </button>
+	                	</td>
+    				</tr>
+    				</form>
+			<?php
+	          	}
+	        ?>
 		  </tbody>
 		</table>
 	</div>
