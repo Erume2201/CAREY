001337fb1aac3a4327fe9fb0a207d1b929096075
Erume2201 <135057750+Erume2201@users.noreply.github.com>
2023-06-28 11:08:36 -0600

    algho

--- modules/moduloCreditos/subnavbarCreditos.php
+++ modules/moduloCreditos/subnavbarCreditos.php
@@ -1,7 +1,7 @@
-<br><br><hr>
+<br><br><br><hr>
 <ul class="nav nav-tabs">
   <li class="nav-item">
-    <a class="nav-link" aria-current="page" href="index.php?module=creditos">Consultar</a>
+    <a class="nav-link" aria-current="page" href="index.php?module=creditos">Consultar fecha</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="index.php?module=historialCreditos">Historial de pagos</a>
