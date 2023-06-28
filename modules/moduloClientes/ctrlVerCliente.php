23a9c9b01bac3a89787adcfb34a126556d4a94d6
Jonahcr7 <jr.custodio0178@gmail.com>
2023-06-27 20:03:34 -0600

    deleteCliente
    seguimiento al botón de eliminar cliente, aún no funciona pq la tabla de crédito

--- modules/moduloClientes/ctrlVerCliente.php
+++ modules/moduloClientes/ctrlVerCliente.php
@@ -1,19 +1,38 @@
+
 <!--Este módulo controlará los botones de ver, editar y eliminar-->
 
-<?php
-if (isset($_POST['dato'])) {
-		$dato = $_POST['dato'];
-		//echo $dato;
-		$SQL  = "DELETE FROM usuarios WHERE id_usuarios = '$dato';";
-	    $resultadoDelete = EliminarDato($SQL);
-	    if ($resultadoDelete) {
-	        echo "<script>window.location = '../karey/index.php?module=usuario'</script>";
-	        ?>
-	        	<div class="alert alert-danger" role="alert">
-					Usuario Eliminado
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require '../../controller/php/CRUD.php'; 
+
+	if (isset($_POST['delete'])) {
+		$dato = $_POST['datoDelete'];
+		$querySaldo = "SELECT total FROM creditos WHERE cliente_id = '$dato'";
+		if ($querySaldo != 0) {
+			?>
+			   	<div class="alert alert-danger" role="alert">
+					  No es posible eliminar al cliente seleccionado debido a que tiene un crédito pendiente.
 				</div>
-	        <?php
-	    }else{
-	        echo "<script>window.location = '../karey/index.php?module=usuario'</script>";
-	    }
-}
+			<?php
+		} else {
+			$SQL = "DELETE FROM cliente WHERE id_cliente = '$dato'";
+			$resultado = EliminarDato($SQL);
+			if ($resultado) {
+				?>
+					<div class="alert alert-success" role="alert"> El cliente fue eliminado exitosamente.
+					</div>
+				<?php 
+				echo "<script>window.location = '../../index.php?module=clientes&Delete=Borrado'</script>";
+			} else {
+				echo "<script>window.location = '../../index.php?module=clientes&Delete=NoBorrado'</script>";
+				?>
+					<div class="alert alert-danger" role="alert">
+				  	Ocurrió un error, el cliente no pudo ser eliminado.
+					</div>
+				<?php
+			}
+		}
+	}
+?>
--- modules/moduloClientes/subnavbarClientes.php
+++ modules/moduloClientes/subnavbarClientes.php
@@ -1,4 +1,4 @@
-<br><br><hr>
+<br><br><br><br>
 <ul class="nav nav-tabs">
   <li class="nav-item">
     <a class="nav-link" aria-current="page" href="index.php?module=clientes">Clientes</a>
