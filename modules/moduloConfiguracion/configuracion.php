dd19d763d16653ac0c8807f44f22de0c927a6153
Jonahcr7 <jr.custodio0178@gmail.com>
2023-06-30 23:46:02 -0600

    configuracion

--- modules/moduloConfiguracion/configuracion.php
+++ modules/moduloConfiguracion/configuracion.php
@@ -1,38 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Asignación de días</title>
+</head>
+<body>
+    <!--Creación del módulo de Configuración-->
+    <div class="container col-6">
+        <br><br><br><br><br>
+        <h1>Asignación de días</h1>
+        <hr>
+        <h6>Seleccione los días en los que se hará el corte</h6>
+        <?php
+        include("modules/moduloConfiguracion/procesar.php");
+        ?>
+        <form method="POST">
+            <label for="desde">Desde:</label>
+            <select name="desde" id="desde" required>
+                <option value="Monday">Lunes</option>
+                <option value="Tuesday">Martes</option>
+                <option value="Wednesday">Miércoles</option>
+                <option value="Thursday">Jueves</option>
+                <option value="Friday">Viernes</option>
+                <option value="Saturday">Sábado</option>
+                <option value="Sunday">Domingo</option>
+            </select>
+            <br><br>
+            <label for="hasta">Hasta:</label>
+            <select name="hasta" id="hasta" required>
+                <option value="Monday">Lunes</option>
+                <option value="Tuesday">Martes</option>
+                <option value="Wednesday">Miércoles</option>
+                <option value="Thursday">Jueves</option>
+                <option value="Friday">Viernes</option>
+                <option value="Saturday">Sábado</option>
+                <option value="Sunday">Domingo</option>
+            </select>
+            <br><br>
+            <input type="submit" value="Guardar cambios">
+        </form>
+        <hr>
+        <h1>Alguna otra configuración...</h1>
+    </div>
+</body>
+</html>
 
-<!--Creación del módulo de Configuración-->
-<div class="container col-6">
-	<br><br><br><br><br>
-	<h1>Asignación de días</h1>
-	<hr>
-	<h6>Seleccione los días en los que se hará el corte</h6>
-	<?php
-	include_once("modules/moduloConfiguracion/procesar.php");
-	?>
-	<form method="POST">
-        <label for="desde">Desde:</label>
-        <select name="desde" required>
-            <option value="Monday">Lunes</option>
-            <option value="Tuesday">Martes</option>
-            <option value="Wednesday">Miércoles</option>
-            <option value="Thursday">Jueves</option>
-            <option value="Friday">Viernes</option>
-            <option value="Saturday">Sábado</option>
-            <option value="Sunday">Domingo</option>
-        </select>
-        <br><br>
-        <label for="hasta">Hasta:</label>
-        <select name="hasta" required>
-            <option value="Monday">Lunes</option>
-            <option value="Tuesday">Martes</option>
-            <option value="Wednesday">Miércoles</option>
-            <option value="Thursday">Jueves</option>
-            <option value="Friday">Viernes</option>
-            <option value="Saturday">Sábado</option>
-            <option value="Sunday">Domingo</option>
-        </select>
-        <br><br>
-        <input type="submit" value="Guardar cambios">
-    </form>
-    <hr>
-    <h1>Alguna otra configuración...</h1>
-</div>
--- modules/moduloConfiguracion/procesar.php
+++ modules/moduloConfiguracion/procesar.php
@@ -1,23 +1,18 @@
-
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obtención de los días de la semana seleccionados
-    $desdeDia = $_POST['desde'];
-    $hastaDia = $_POST['hasta'];
-
+function obtenerFechasCorte($desdeDia, $hastaDia) {
     // Obtención de la fecha actual
+    date_default_timezone_set('America/Mexico_City');
     $fechaActual = date('Y-m-d');
-
-    echo "Fecha actual: " . $fechaActual . "<br>";
-    echo "Fecha DESDE: " . $desdeDia . "<br>";
-    echo "Fecha HASTA: " . $hastaDia . "<br>";
+    $timestamp = strtotime($fechaActual);
+    $diaActual = date('l', $timestamp);
 
     // Cálculo de las fechas de inicio y fin en función de los días seleccionados
-    $fecha_inicio = date('Y-m-d', strtotime("last $desdeDia", strtotime($fechaActual)));
+    if ($desdeDia == $diaActual) {
+        $fecha_inicio = $fechaActual; 
+    } else {
+        $fecha_inicio = date('Y-m-d', strtotime("last $desdeDia", strtotime($fechaActual)));
+    }
     $fecha_fin = date('Y-m-d', strtotime("next $hastaDia", strtotime($fechaActual)));
-
-    echo "Fecha DESDE: " . $fecha_inicio . "<br>";
-    echo "Fecha HASTA: " . $fecha_fin . "<br>";
 
     // Obtención de los timestamps de las fechas DESDE y HASTA
     $timestamp_desde = strtotime($fecha_inicio);
@@ -26,18 +21,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Calculamos la diferencia en días
     $diferencia_dias = ($timestamp_hasta - $timestamp_desde) / (60 * 60 * 24);
 
-    // Verificamos que la diferencia entre DESDE y HASTA sea de 7 dias calendarios
+    // Verificamos que la diferencia entre DESDE y HASTA sea de 7 días calendario
     if ($diferencia_dias != 6) {
-        echo '<div class="alert alert-danger" role="alert">
-                La diferencia de días entre DESDE y HASTA debe ser de 7 días calendario.
-              </div>';
+        return false;
     } else {
         // Almacenamos las fechas en las variables $desde y $hasta
         $desde = $fecha_inicio;
         $hasta = $fecha_fin;
-        echo '<div class="alert alert-success" role="alert">Las fechas de corte se guardaron correctamente.</div>';
+        return array('desde' => $desde, 'hasta' => $hasta);
+    }
+}
+
+// Ejemplo de uso:
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $desdeDia = $_POST['desde'];
+    $hastaDia = $_POST['hasta'];
+
+    $fechasCorte = obtenerFechasCorte($desdeDia, $hastaDia);
+
+    if ($fechasCorte) {
+        $desde = $fechasCorte['desde'];
+        $hasta = $fechasCorte['hasta'];
+        echo '<div class="alert alert-success" role="alert">Las fechas de corte DESDE (' . $desde . ') y HASTA (' . $hasta . ') se guardaron correctamente.</div>';
+    } else {
+        echo '<div class="alert alert-danger" role="alert">La diferencia de días entre las fechas DESDE y HASTA debe ser de 7 días calendario.</div>';
     }
 }
 ?>
-
-
