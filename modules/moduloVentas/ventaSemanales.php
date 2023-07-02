d938ea0b68767ccfe81953d592b4a93aa850f4e3
Johann <hernandezjohann288@gmail.com>
2023-07-02 14:15:59 -0600

    ventas Semanales

--- modules/moduloVentas/ventaSemanales.php
+++ modules/moduloVentas/ventaSemanales.php
@@ -7,17 +7,28 @@
     <div class="col-10">
         <div class="accordion" id="">
             <?php
-            $SQL = "SELECT id_cliente, nombre_cliente, nombre_negocio, telefono_cliente FROM cliente;";
+            // Obtener los valores enviados por el formulario
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+            $SQL = "SELECT creditos.cliente_id, cliente.nombre_cliente, ventas.credito_id ,
+            SUM(ventas.total_venta)AS Total_compra
+            FROM creditos
+            JOIN ventas ON creditos.id_creditos= ventas.credito_id
+            JOIN cliente ON cliente.id_cliente= creditos.cliente_id
+            GROUP BY creditos.cliente_id;";
             $resultado = Consulta($SQL);
             foreach ($resultado as $cliente) {
             ?>
                 <div class="accordion-item">
                     <h2 class="accordion-header">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo $cliente['id_cliente']; ?>" aria-expanded="false" aria-controls="<?php echo $cliente['id_cliente']; ?>">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" 
+                        data-bs-target="#<?php echo $cliente['cliente_id']; ?>" aria-expanded="false" 
+                        aria-controls="<?php echo $cliente['cliente_id']; ?>">
                             <?php echo $cliente['nombre_cliente']; ?>
                         </button>
                     </h2>
-                    <div id="<?php echo $cliente['id_cliente']; ?>" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div id="<?php echo $cliente['cliente_id']; ?>" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
                         <div class="accordion-body">
                             <div class="table-responsive">
                                 <table class="table table-bordered" id="tablaSemanales" style="margin-left: 20px; margin-right: 20px;">
@@ -38,15 +49,15 @@
                                     <tbody>
                                         <?php
                                         $SQL2 = "SELECT ventas.id_ventas,  cliente.nombre_cliente,
-                                   cliente.municipio, documentos.nombre, 
-                                   informacion_venta.cantidad, informacion_venta.sub_total, 
-                                   ventas.total_venta,  creditos.total,  creditos.estatus 
-                                FROM documentos
-                                JOIN informacion_venta ON documentos.id_articulo_documetos=informacion_venta.documentos_id
-                                JOIN ventas ON ventas.id_ventas = informacion_venta.ventas_id
-                                JOIN creditos ON creditos.id_creditos=ventas.credito_id
-                                JOIN cliente ON  cliente.id_cliente= creditos.cliente_id
-                                WHERE cliente.nombre_cliente = '". $cliente['nombre_cliente']."';";
+                                        cliente.municipio, documentos.nombre, 
+                                        informacion_venta.cantidad, informacion_venta.sub_total, 
+                                        ventas.total_venta,  creditos.total,  creditos.estatus 
+                                     FROM documentos
+                                     JOIN informacion_venta ON documentos.id_articulo_documetos=informacion_venta.documentos_id
+                                     JOIN ventas ON ventas.id_ventas = informacion_venta.ventas_id
+                                     JOIN creditos ON creditos.id_creditos=ventas.credito_id
+                                     JOIN cliente ON  cliente.id_cliente= creditos.cliente_id
+                                     WHERE cliente.id_cliente = '".$cliente['cliente_id']."'";
                                         $resultado = Consulta($SQL2);
                                         foreach ($resultado as $fila) {
                                         ?>
@@ -67,6 +78,18 @@
                                         }
                                         ?>
                                     </tbody>
+                                    <?php
+                                    $documentos = "SELECT SUM(informacion_venta.cantidad) AS cantidadDoc  FROM informacion_venta
+                                    JOIN ventas ON ventas.id_ventas=informacion_venta.ventas_id
+                                    JOIN creditos ON creditos.id_creditos= ventas.credito_id
+                                    WHERE creditos.cliente_id = '".$cliente['cliente_id']."';";
+                                    $respuesta = Consulta($documentos);
+                                    $respuesta[0]['cantidadDoc'];
+                                    ?>
+                                    <tr>
+                                        <td> Total de la semana: $<?php echo $cliente['Total_compra']; ?></td>
+                                        <td id="CantidaTabla">Cantidad de documentos: <?php echo $respuesta[0]['cantidadDoc'];?></td>
+                                    </tr>
                                 </table>
                             </div>
                         </div>
