2a073f2d200d61926cb7e31a682b5d2160fbcf6a
Jonahcr7 <jr.custodio0178@gmail.com>
2023-07-04 13:15:17 -0600

    fechaCorte

--- modules/moduloConfiguracion/configuracion.php
+++ modules/moduloConfiguracion/configuracion.php
@@ -11,33 +11,37 @@
         <h1>Asignación de días</h1>
         <hr>
         <h6>Seleccione los días en los que se hará el corte</h6>
-        <?php
-        include("modules/moduloConfiguracion/procesar.php");
-        ?>
         <form method="POST">
-            <label for="desde">Desde:</label>
-            <select name="desde" id="desde" required>
-                <option value="Monday">Lunes</option>
-                <option value="Tuesday">Martes</option>
-                <option value="Wednesday">Miércoles</option>
-                <option value="Thursday">Jueves</option>
-                <option value="Friday">Viernes</option>
-                <option value="Saturday">Sábado</option>
-                <option value="Sunday">Domingo</option>
-            </select>
-            <br><br>
+            <?php include("modules/moduloConfiguracion/procesar.php"); ?>
+            <div class="form-group">
+                <label for="desde">Desde:</label>
+                <select name="desde" id="desde" required>
+                    <option value="Monday">Lunes</option>
+                    <option value="Tuesday">Martes</option>
+                    <option value="Wednesday">Miércoles</option>
+                    <option value="Thursday">Jueves</option>
+                    <option value="Friday">Viernes</option>
+                    <option value="Saturday">Sábado</option>
+                    <option value="Sunday">Domingo</option>
+                </select>
+            </div>
+            <br>
+            <div class="form-group">
             <label for="hasta">Hasta:</label>
-            <select name="hasta" id="hasta" required>
-                <option value="Monday">Lunes</option>
-                <option value="Tuesday">Martes</option>
-                <option value="Wednesday">Miércoles</option>
-                <option value="Thursday">Jueves</option>
-                <option value="Friday">Viernes</option>
-                <option value="Saturday">Sábado</option>
-                <option value="Sunday">Domingo</option>
-            </select>
-            <br><br>
-            <input type="submit" value="Guardar cambios">
+                <select name="hasta" id="hasta" required>
+                    <option value="Monday">Lunes</option>
+                    <option value="Tuesday">Martes</option>
+                    <option value="Wednesday">Miércoles</option>
+                    <option value="Thursday">Jueves</option>
+                    <option value="Friday">Viernes</option>
+                    <option value="Saturday">Sábado</option>
+                    <option value="Sunday">Domingo</option>
+                </select>
+            </div>
+            <br>
+            <div class="form-group">
+                <input type="submit" name="enviar" value="Guardar cambios">
+            </div>
         </form>
         <hr>
         <h1>Alguna otra configuración...</h1>
--- modules/moduloConfiguracion/procesar.php
+++ modules/moduloConfiguracion/procesar.php
@@ -1,49 +1,59 @@
+
+<!--Este es el controlador para agregar un nuevo cliente a la BD-->
+
 <?php
-function obtenerFechasCorte($desdeDia, $hastaDia) {
-    // Cambiamos la zona por default y obtemos el día de la fecha actual
-    date_default_timezone_set('America/Mexico_City');
-    $fechaActual = date('Y-m-d');
-    $timestamp = strtotime($fechaActual);
-    $diaActual = date('l', $timestamp);
+    // Nuestro IF principal verifica si se ha enviado el formulario y se ha hecho clic en "enviar".
+    if (isset($_POST['enviar'])) {
+        // Colocaremos otro if para que en caso de que algún campo está vacío se envíe una alerta.
+        if (empty($_POST["desde"]) || empty($_POST["hasta"])) {
+            ?>
+                <div class="alert alert-danger" role="alert">Oops, algunos campos no fueron seleccionados.</div>
+            <?php
+        } else {
+            /* Asignamos los valores de los campos del formulario (accediendo a través del arreglo '$_POST') a variables específicas. */
+            $desdeDia = $_POST["desde"];
+            $hastaDia = $_POST["hasta"];
 
-    // Cálculo de las fechas de inicio y fin en función de los días seleccionados
-    if ($desdeDia == $diaActual) {
-        $fecha_inicio = $fechaActual; 
-    } else {
-        $fecha_inicio = date('Y-m-d', strtotime("last $desdeDia", strtotime($fechaActual)));
+            // Cambiamos la zona por default y obtemos el día de la fecha actual
+            date_default_timezone_set('America/Mexico_City');
+            $fechaActual = date('Y-m-d');
+            $timestamp = strtotime($fechaActual);
+            $diaActual = date('l', $timestamp);
+
+            // Cálculo de las fechas de inicio y fin en función de los días seleccionados
+            if ($desdeDia == $diaActual) {
+                $fecha_inicio = $fechaActual; 
+            } else {
+                $fecha_inicio = date('Y-m-d', strtotime("last $desdeDia", strtotime($fechaActual)));
+            }
+            $fecha_fin = date('Y-m-d', strtotime("next $hastaDia", strtotime($fechaActual)));
+
+            // Obtención de los timestamps de las fechas DESDE y HASTA
+            $timestamp_desde = strtotime($fecha_inicio);
+            $timestamp_hasta = strtotime($fecha_fin);
+
+            // Calculamos la diferencia en días
+            $diferencia_dias = ($timestamp_hasta - $timestamp_desde) / (60 * 60 * 24);
+
+            // Verificamos que la diferencia entre DESDE y HASTA sea de 7 dias calendarios
+            if ($diferencia_dias != 6) {
+                echo '<div class="alert alert-danger" role="alert">La diferencia de dias entre las fechas DESDE (' . $fecha_inicio . ') y HASTA (' . $fecha_fin . ') deben ser de 7 dias calendario.</div>';
+            } else {
+                // Almacenamos las fechas en las variables $desde y $hasta
+                $desde = $fecha_inicio;
+                $hasta = $fecha_fin;
+                // Ahora insertaremos los datos en la BD.
+                $SQL = "INSERT INTO corte (desde, hasta, usuarios_id) VALUES ('$desde', '$hasta', 1)";
+                $resultado = InsertarDato($SQL);
+                if ($resultado) {
+                    echo '<div class="alert alert-success" role="alert">Las fechas de corte DESDE (' . $fecha_inicio . ') y HASTA (' . $fecha_fin . ') se guardaron correctamente.</div>';
+                } else {
+                    ?>
+                        <div class="alert alert-danger" role="alert">Algo salió mal, por favor intentelo de nuevo.</div>
+                    <?php
+
+                }
+            }
+        }
     }
-    $fecha_fin = date('Y-m-d', strtotime("next $hastaDia", strtotime($fechaActual)));
-
-    // Obtención de los timestamps de las fechas DESDE y HASTA
-    $timestamp_desde = strtotime($fecha_inicio);
-    $timestamp_hasta = strtotime($fecha_fin);
-
-    // Calculamos la diferencia en días
-    $diferencia_dias = ($timestamp_hasta - $timestamp_desde) / (60 * 60 * 24);
-
-    // Verificamos que la diferencia entre DESDE y HASTA sea de 7 días calendario
-    if ($diferencia_dias != 6) {
-        return false;
-    } else {
-        // Almacenamos las fechas en las variables $desde y $hasta
-        $desde = $fecha_inicio;
-        $hasta = $fecha_fin;
-        return array('desde' => $desde, 'hasta' => $hasta);
-    }
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $desdeDia = $_POST['desde'];
-    $hastaDia = $_POST['hasta'];
-
-    $fechasCorte = obtenerFechasCorte($desdeDia, $hastaDia);
-
-    if ($fechasCorte) {
-        $desde = $fechasCorte['desde'];
-        $hasta = $fechasCorte['hasta'];
-        echo '<div class="alert alert-success" role="alert">Las fechas de corte DESDE (' . $desde . ') y HASTA (' . $hasta . ') se guardaron correctamente.</div>';
-    } else {
-        echo '<div class="alert alert-danger" role="alert">La diferencia de días entre las fechas DESDE y HASTA debe ser de 7 días calendario.</div>';
-    }
-}
 ?>
