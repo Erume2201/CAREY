f6b7a9c833f29662a3c01e013e04881ac805671f
Erume2201 <135057750+Erume2201@users.noreply.github.com>
2023-07-04 19:20:00 -0600

    modulo de creditos terminado

---
+++ controller/php/controlCreditos/modalCreditoVencido.php
@@ -0,0 +1,70 @@
+<!-- Modal -->
+<div class="modal fade" id="CreditosVencidos<?php echo $fila2['id_cliente']; ?>" tabindex="-1"
+    aria-labelledby="CreditosVencidos<?php echo $fila2['id_cliente']; ?>Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="CreditosVencidos<?php echo $fila2['id_cliente']; ?>Label" style="">PAGAR
+                    CREDITO</h4>
+            </div>
+            <div class="modal-body">
+                <!--Inicio Formulario-->
+                <form action="controller/php/controlCreditos/pagarCredito.php" method="POST">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1" class="form-label fw-bold">Id Cliente</label>
+                        <input type="text" class="form-control" name="idCliente" id="idCliente" placeholder=""
+                            value="<?php echo $fila2['id_cliente']; ?>" disabled>
+                        <label for="exampleInputEmail1" class="form-label fw-bold">Nombre Cliente</label>
+                        <input type="text" class="form-control" name="nombreCliente" id="nombreCliente" placeholder=""
+                            value="<?php echo $fila2['nombre_cliente']; ?>" disabled>
+                        <!-- variables a enviar idCliente, nombreCliente-->
+                        <input type="hidden" class="form-control" name="idCliente" id="idCliente" placeholder=""
+                            value="<?php echo $fila2['id_cliente']; ?>">
+                        <input type="hidden" class="form-control" name="nombreCliente" id="nombreCliente" placeholder=""
+                            value="<?php echo $fila2['nombre_cliente']; ?>">
+                        <!-- termina variable -->
+                    </div>
+                    <div class="form-group">
+                        <br>
+                        <label for="exampleInputEmail1" class="form-label fw-bold">Numero de telefono</label>
+                        <input type="text" class="form-control" name="numeroTelefono" id="numeroTelefono" placeholder=""
+                            maxlength="10" oninput="validarNumeros(this)"
+                            value="<?php echo $fila2['telefono_cliente']; ?>" disabled>
+                        <input type="hidden" class="form-control" name="numeroTelefono" id="numeroTelefono"
+                            placeholder="" maxlength="10" oninput="validarNumeros(this)"
+                            value="<?php echo $fila2['telefono_cliente']; ?>">
+
+                    </div>
+                    <div class="form-group">
+                        <br>
+                        <label for="exampleInputEmail1" class="form-label fw-bold">Empresa</label>
+                        <input type="text" class="form-control" name="empresa" id="empresa" placeholder=""
+                            value="<?php echo $fila2['nombre_negocio']; ?>" disabled>
+                        <input type="hidden" class="form-control" name="empresa" id="empresa" placeholder=""
+                            value="<?php echo $fila2['nombre_negocio']; ?>">
+
+                    </div>
+                    <div class="form-group">
+                        <br>
+                        <label for="exampleInputEmail1" class="form-label fw-bold">Pago total del crédito</label>
+                        <input type="text" class="form-control" name="totalPago" id="totalPago" placeholder=""
+                            value="<?php echo $fila2['deuda']; ?>" disabled>
+                        <input type="hidden" class="form-control" name="totalPago" id="totalPago" placeholder=""
+                            value="<?php echo $fila2['deuda']; ?>">
+
+                        <!-- Variables ocultas -->
+                        <input type="hidden" class="bt-5 form-control" name="fechaInicioConsulta"
+                            value="<?php echo $fila2['fecha_ini']; ?>">
+                        <input type="hidden" class="bt-5 form-control" name="fechaFinConsulta"
+                            value="<?php echo $fila2['fecha_fin']; ?>">
+                    </div>
+                    <!--Fin Formulario-->
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                <button type="submit" class="btn btn-primary">Pagar</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
--- modules/moduloCreditos/historial.php
+++ modules/moduloCreditos/historial.php
@@ -1,7 +1,21 @@
 <?php 
- 
-    $fechaInicio = '2023-06-27';
-    $fechaFin= '2023-07-03';
+    #consultar fecha actual
+    $SQLFechaActual = "SELECT desde, hasta
+    FROM corte
+    ORDER BY hasta DESC
+    LIMIT 1;";
+ #Consulta de creditos de la semana actual y asignar
+    $consultaFecha = Consulta($SQLFechaActual);
+    $fechaInicio = $consultaFecha[0]['desde'];
+    $fechaFin = $consultaFecha[0]['hasta'];
+
+    echo $fechaInicio .'<br>';
+    echo $fechaFin;
+   
+/**
+ * El siguiente método consulta los datos de la semana actual
+ * para mandarlo a la tabla.
+ */
     $SQL = "SELECT cli.id_cliente, cli.nombre_cliente,cre.estatus, 
     cli.telefono_cliente, cli.nombre_negocio, cre.fecha ,COUNT(cre.id_creditos) 
     AS total_creditos, SUM(cre.total) AS total_ventas
@@ -10,89 +24,313 @@
     Where (cre.fecha >='$fechaInicio'  AND cre.fecha <= '$fechaFin') AND cre.estatus='pendiente'
     GROUP BY cli.id_cliente, cli.nombre_cliente, cli.telefono_cliente, cli.nombre_negocio;";
     $consultarDatos2 = Consulta($SQL);
-     ?>
+   
+ /**
+  * Consulta datos vencidos de la db
+  * creada el 04-07-2023
+  */
+    $SQLVencidos = "SELECT cli.id_cliente, cli.nombre_cliente, cre.estatus, cli.telefono_cliente, 
+    cli.nombre_negocio, COUNT(cre.id_creditos) AS total_creditos, 
+    DATE(informes.fecha_inicio)  AS fecha_ini, DATE(informes.fecha_final) AS fecha_fin, SUM(cre.total) AS deuda
+    FROM cliente cli
+    JOIN creditos cre ON cli.id_cliente = cre.cliente_id
+    JOIN ventas ve ON ve.credito_id = cre.id_creditos
+    JOIN informacion_venta ON ve.id_ventas = informacion_venta.ventas_id
+    JOIN documentos ON documentos.id_articulo_documetos = informacion_venta.documentos_id
+    JOIN informes_detalles ON documentos.id_articulo_documetos = informes_detalles.documentos_id
+    JOIN informes ON informes.id_informes = informes_detalles.informe_id
+    WHERE cre.fecha NOT BETWEEN '$fechaInicio' AND '$fechaFin' AND cre.estatus='pendiente'
+    GROUP BY cli.id_cliente, cli.nombre_cliente, cre.estatus, cli.telefono_cliente, cli.nombre_negocio, DATE(informes.fecha_inicio), DATE(informes.fecha_final);";
+    $consultarDatosVencidos = Consulta($SQLVencidos);
 
- <!--Buscador-->
- <input class="form-control me-2 light-table-filter" data-table="table_id" type="text"
-  placeholder="Ingresa el nombre del cliente" style="text-align: center" id="buscadorBloque">
-<!-- Buscador-->
-<!-- Tabla -->
-<h3>Lista de creditos de los clientes que compraron documentos en la semana actual</h3>
-<div class="container-do row">
-    <div class="container col-8">
-        <table class="table table-hover table-sm table-bordered table_id" id="table">
-            <thead class="table-dark text-center">
-                <tr>
-                    <th>Id Cliente</th>
-                    <th>Nombre Cliente</th>
-                    <th>Telefono</th>
-                    <th>Empresa</th>
-                    <th>Estatus</th>
-                    <th>Fecha inicio</th>
-                    <th>Fecha fin</th>
-                    <th>N° Documentos</th>
-                    <th>Total</th>
-                    <th>Pagar</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($consultarDatos2 as $fila2) { ?>
-                <tr>
-                    <td><?php echo $fila2['id_cliente']; ?></td>
-                    <td><?php echo $fila2['nombre_cliente']; ?></td>
-                    <td><?php echo $fila2['telefono_cliente']; ?></td>
-                    <td><?php echo $fila2['nombre_negocio']; ?></td>
-                    <td><?php echo $fila2['estatus']; ?></td>
-                    <td><?php echo $fechaInicio ?></td>
-                    <td><?php echo $fechaFin ?></td>
-                    <td><?php echo $fila2['total_creditos']; ?></td>
-                    <td><?php echo $fila2['total_ventas']; ?></td>
-                    <td> <button type="button" class="btn" data-bs-toggle="modal"
-                            data-bs-target="#exampleModal<?php echo $fila2['id_cliente']; ?>"
-                            style="width: 40px; height: 40px; display: flex; justify-content: center; align-items: center;">
-                            <img src="assets/image/pagar-credito.png" width="35" height="35">
-                        </button></td>
-                </tr>
+ /**
+  * Consultar datos pagados de cada cliente durante la semana
+  * consulta creada 04-07-2023
+  */
+ $SQLpagados = "SELECT cli.id_cliente, cli.nombre_cliente,cre.estatus, 
+ cli.telefono_cliente, cli.nombre_negocio, cre.fecha ,COUNT(cre.id_creditos) 
+ AS total_creditos, SUM(cre.total) AS total_ventas
+ FROM cliente cli
+ LEFT JOIN creditos cre ON cli.id_cliente = cre.cliente_id
+ Where (cre.fecha >='2023-06-28'  AND cre.fecha <= '2023-07-05') AND cre.estatus='pagado'
+ GROUP BY cli.id_cliente, cli.nombre_cliente, cli.telefono_cliente, cli.nombre_negocio;";
+ $consultarDatosPagados = Consulta($SQLpagados);
+  ?>
 
-                <?php 
+
+<!-- Acordeon -->
+<div class="accordion accordion-flush" id="accordionFlushExample">
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                Creditos de la semana actual
+            </button>
+        </h2>
+        <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">
+
+                <!-- Tabla 01 para documentos vendidos en la semana -->
+                <!--Buscador-->
+                <div class="row">
+                    <div class="col-3">
+                        <!-- Contenido de la primera columna -->
+                    </div>
+                    <div class="col-6">
+                        <!-- Contenido de la segunda columna -->
+                    </div>
+                    <div class="col-3">
+                        <!-- Contenido de la tercera columna -->
+                        <center><label class="form-label fw-bold">Buscar cliente</label></center>
+                        <input class="form-control me-2 light-table-filter" data-table="table_id" type="text"
+                            placeholder="Ingresa el nombre del cliente" style="text-align: center" id="buscadorBloque">
+                    </div>
+                </div>
+
+
+                <!-- Buscador-->
+                <!-- Tabla -->
+                <h3>Lista de creditos de los clientes que compraron documentos en la semana actual</h3>
+                <div class="container-do row">
+                    <div class="container col-8">
+                        <table class="table table-hover table-sm table-bordered table_id" id="table">
+                            <thead class="table-dark text-center">
+                                <tr>
+                                    <th>Id Cliente</th>
+                                    <th>Nombre Cliente</th>
+                                    <th>Telefono</th>
+                                    <th>Empresa</th>
+                                    <th>Estatus</th>
+                                    <th>Fecha inicio</th>
+                                    <th>Fecha fin</th>
+                                    <th>N° de créditos</th>
+                                    <th>Total</th>
+                                    <th>Pagar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($consultarDatos2 as $fila2) { ?>
+                                <tr>
+                                    <td><?php echo $fila2['id_cliente']; ?></td>
+                                    <td><?php echo $fila2['nombre_cliente']; ?></td>
+                                    <td><?php echo $fila2['telefono_cliente']; ?></td>
+                                    <td><?php echo $fila2['nombre_negocio']; ?></td>
+                                    <td><?php echo $fila2['estatus']; ?></td>
+                                    <td><?php echo $fechaInicio ?></td>
+                                    <td><?php echo $fechaFin ?></td>
+                                    <td><?php echo $fila2['total_creditos']; ?></td>
+                                    <td><?php echo $fila2['total_ventas']; ?></td>
+                                    <td> <button type="button" class="btn" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModal<?php echo $fila2['id_cliente']; ?>"
+                                            style="width: 40px; height: 40px; display: flex; justify-content: center; align-items: center;">
+                                            <img src="assets/image/pagar-credito.png" width="35" height="35">
+                                        </button></td>
+                                </tr>
+
+                                <?php 
                 include("controller/php/controlCreditos/modalCliente.php");
             } ?>
-            </tbody>
-        </table>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- Termina Tabla 01 para documentos vendidos en la semana -->
+            </div>
+        </div>
     </div>
+
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                Creditos vencidos
+            </button>
+        </h2>
+        <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">
+
+                <!-- Tabla 02 para documentos vencidos en la semana -->
+                <!--Buscador-->
+                <div class="row">
+                    <div class="col-3">
+                        <!-- Contenido de la primera columna -->
+                    </div>
+                    <div class="col-6">
+                        <!-- Contenido de la segunda columna -->
+                    </div>
+                    <div class="col-3">
+                        <!-- Contenido de la tercera columna -->
+                        <center><label class="form-label fw-bold">Buscar cliente</label></center>
+                        <input class="form-control me-2 light-table-filter" data-table="table_vencidos" type="text"
+                            placeholder="Ingresa el nombre del cliente" style="text-align: center" id="buscadorBloque">
+                    </div>
+                </div>
+
+
+                <!-- Buscador-->
+                <!-- Tabla -->
+                <h3>Lista de creditos vencidos</h3>
+                <div class="container-do row">
+                    <div class="container col-8">
+                        <table class="table table-hover table-sm table-bordered table_vencidos" id="table">
+                            <thead class="table-dark text-center">
+                                <tr>
+                                    <th>Id Cliente</th>
+                                    <th>Nombre Cliente</th>
+                                    <th>Telefono</th>
+                                    <th>Empresa</th>
+                                    <th>Estatus</th>
+                                    <th>Fecha inicio</th>
+                                    <th>Fecha fin</th>
+                                    <th>N° Documentos</th>
+                                    <th>Total</th>
+                                    <th>Pagar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($consultarDatosVencidos as $fila2) { ?>
+                                <tr>
+                                    <td><?php echo $fila2['id_cliente']; ?></td>
+                                    <td><?php echo $fila2['nombre_cliente']; ?></td>
+                                    <td><?php echo $fila2['telefono_cliente']; ?></td>
+                                    <td><?php echo $fila2['nombre_negocio']; ?></td>
+                                    <td><?php echo $fila2['estatus']; ?></td>
+                                    <td><?php echo $fila2['fecha_ini']; ?></td>
+                                    <td><?php echo $fila2['fecha_fin']; ?></td>
+                                    <td><?php echo $fila2['total_creditos']; ?></td>
+                                    <td><?php echo $fila2['deuda']; ?></td>
+                                    <td> <button type="button" class="btn" data-bs-toggle="modal"
+                                            data-bs-target="#CreditosVencidos<?php echo $fila2['id_cliente']; ?>"
+                                            style="width: 40px; height: 40px; display: flex; justify-content: center; align-items: center;">
+                                            <img src="assets/image/pagar-credito.png" width="35" height="35">
+                                        </button></td>
+                                </tr>
+
+                                <?php 
+                include("controller/php/controlCreditos/modalCreditoVencido.php");
+            } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- Termina Tabla 02 para documentos vendidos en la semana -->
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Inicia item-03 -->
+    
+    <div class="accordion-item">
+        <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                Creditos pagados
+            </button>
+        </h2>
+        <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">
+
+                <!-- Tabla 02 para documentos vendidos en la semana -->
+                <!--Buscador-->
+                <div class="row">
+                    <div class="col-3">
+                        <!-- Contenido de la primera columna -->
+                    </div>
+                    <div class="col-6">
+                        <!-- Contenido de la segunda columna -->
+                    </div>
+                    <div class="col-3">
+                        <!-- Contenido de la tercera columna -->
+                        <center><label class="form-label fw-bold">Buscar cliente</label></center>
+                        <input class="form-control me-2 light-table-filter" data-table="table_creditosPagados" type="text"
+                            placeholder="Ingresa el nombre del cliente" style="text-align: center" id="buscadorBloque">
+                    </div>
+                </div>
+
+
+                <!-- Buscador-->
+                <!-- Tabla -->
+                <h3>Lista de creditos pagados</h3>
+                <div class="container-do row">
+                    <div class="container col-8">
+                        <table class="table table-hover table-sm table-bordered table_creditosPagados" id="table">
+                            <thead class="table-dark text-center">
+                                <tr>
+                                    <th>Id Cliente</th>
+                                    <th>Nombre Cliente</th>
+                                    <th>Telefono</th>
+                                    <th>Empresa</th>
+                                    <th>Estatus</th>
+                                    <th>Fecha inicio</th>
+                                    <th>Fecha fin</th>
+                                    <th>N° de créditos</th>
+                                    <th>Total</th>
+                                 
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($consultarDatosPagados as $fila2) { ?>
+                                <tr>
+                                    <td><?php echo $fila2['id_cliente']; ?></td>
+                                    <td><?php echo $fila2['nombre_cliente']; ?></td>
+                                    <td><?php echo $fila2['telefono_cliente']; ?></td>
+                                    <td><?php echo $fila2['nombre_negocio']; ?></td>
+                                    <td><?php echo $fila2['estatus']; ?></td>
+                                    <td><?php echo $fechaInicio ?></td>
+                                    <td><?php echo $fechaFin ?></td>
+                                    <td><?php echo $fila2['total_creditos']; ?></td>
+                                    <td><?php echo $fila2['total_ventas']; ?></td>   
+                                </tr>
+
+                                <?php 
+            } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- Termina Tabla 02 para documentos vendidos en la semana -->
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Termina item-03-->
 </div>
+</div>
+
 
 
 
 <!-- Alerta de Usuario Modificado -->
 <div>
-  <?php
+    <?php
   if (isset($_GET['Update'])) {
     $status = $_GET['Update'];
     if ($status == "NoPagado") {
   ?>
-      <script>
-        Swal.fire({
-          icon: 'error',
-          title: 'El Usuario no se logro modificar',
-          text: '¡Verifique que halla llenado todos los campos!!!'
-        })
-      </script>
+    <script>
+    Swal.fire({
+        icon: 'error',
+        title: 'El Usuario no se logro modificar',
+        text: '¡Verifique que halla llenado todos los campos!!!'
+    })
+    </script>
     <?php
     } else if($status == "NoPagado1"){
     ?>
-      <script>
-        Swal.fire('¡Error al realizar el pago!', '', 'success')
-      </script>
-  <?php
+    <script>
+    Swal.fire('¡Error al realizar el pago!', '', 'error')
+    </script>
+    <?php
     }else{
         ?>
-         <script>
-        Swal.fire('¡Pago realizado con exito!', '', 'success')
-      </script>
-      <?php
+    <script>
+    Swal.fire('¡Pago realizado con exito!', '', 'success')
+    </script>
+    <?php
 
     }
 }
     ?>
-<script src="controller/javascript/buscador.js"></script>
+    <script src="controller/javascript/buscador.js"></script>
