9f39b94b71ba32f71ab32cd9799ed851a6f1c639
JPech213 <119896157+JPech213@users.noreply.github.com>
2023-07-11 17:27:36 -0600

    Solucion btn copiar
    Se borro xd

--- modules/moduloUsuario/modalConfirmarPass.php
+++ modules/moduloUsuario/modalConfirmarPass.php
@@ -1,7 +1,7 @@
 <!--
 Agrega un modal de confirmación para el cambio de contraseña 
 El modal muestra el nombre de usuario y un campo para la nueva contraseña 
-Incluye un botón para copiar la contraseña y un formulario para confirmar el cambio de contraseña 
+Formulario para confirmar el cambio de contraseña 
 -->
 
 <div class="modal fade" id="confirmar<?php echo $fila['id_usuarios']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -19,8 +19,7 @@ Incluye un botón para copiar la contraseña y un formulario para confirmar el c
         <h5>Contraseña nueva: <h5 id="h5<?php echo $fila['id_usuarios']; ?>"> </h5></h5>
         <div id="copiar<?php echo $fila['id_usuarios']; ?>"></div>
         <br>
-        <h7>Asegurate de guardar la contraseña nueva!!! <button type="button" class="btn btn-dark" value="<?php echo $fila['id_usuarios']; ?>"  onclick="ChecandoModal(event)">
-          Copiar</button></h7>
+        <h7>Asegurate de guardar la contraseña nueva!!!</h7>
         <br>
         <input type="hidden" class="bt-5" id="dato" name="dato" value="<?php echo $fila['id_usuarios']; ?>" autocomplete="off">
         <input type="hidden" class="password-field form-control" name="pass" id="pass<?php echo $fila['id_usuarios']; ?>" readonly>
